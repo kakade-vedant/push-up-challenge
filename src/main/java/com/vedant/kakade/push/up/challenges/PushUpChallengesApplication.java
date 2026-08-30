83906e91346ed3acc123e57bbfecddf7741f9af2
Vedant Kakade <kakadevedant13@gmail.com>
2026-08-30 22:11:12 +0530

    Project Initialization

---
+++ src/main/java/com/vedant/kakade/push/up/challenges/PushUpChallengesApplication.java
@@ -0,0 +1,13 @@
+package com.vedant.kakade.push.up.challenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushUpChallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushUpChallengesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vedant/kakade/push/up/challenges/PushUpChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedant.kakade.push.up.challenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushUpChallengesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
